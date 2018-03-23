092938ebcfaed82d26b9a27579ee79baf3b6051b
Arnold Toth <askywalker.22@gmail.com>
2018-03-23 13:13:40 +0100

    script hozzáadása

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   	echo "rootként futtasd! (sudo sh setup.sh)" 
+   	exit 1
+else
+	echo "update & upgrade"
+	sudo apt update && sudo apt upgrade -y
+	echo "ssh telepítése"
+	sudo apt install openssh-server -y
+	echo "git, ansible telepítése..."
+	sudo apt install git software-properties-common -y
+	sudo apt-add-repository ppa:ansible/ansible -y
+	sudo apt update
+	sudo apt install ansible
+    sudo apt autoclean -y && sudo apt autoremove -y
+    read -sp 'Jelszó: 'sqljelszo
+    sudo ansible-playbook playbook.yml -i hosts -e mysql_root_password=$sqljelszo
+fi
