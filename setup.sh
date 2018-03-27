5d545dbf41062fe52419a71e3196c74466d80344
Arnold Toth <askywalker.22@gmail.com>
2018-03-27 11:03:44 +0200

    apróbb módosítások

--- setup.sh
+++ setup.sh
@@ -3,20 +3,21 @@ RED='\033[0;31m'
 YEL='\033[0;33m'
 GRE='\033[0;37m'
 NC='\033[0m'
+sqljelszo=kalifornia
 if ! [ $(id -u) = 0 ]; then
     echo "${RED}root jog szükséges ${GRE}(sudo sh setup.sh)"
     exit 1
 fi
-    echo "${YEL}rootjog ok!${NC}"
-	echo "${YEL}apt update......${NC}"
+    echo "${YEL}»» rootjog ok!${NC}"
+	echo "${YEL}»» apt update......${NC}"
 	sudo apt update -y
-	echo "${YEL}ssh telepítése......${NC}"
+	echo "${YEL}»» ssh telepítése......${NC}"
 	sudo apt install openssh-server -y
-	echo "${YEL}git, ansible telepítése......${NC}"
+	echo "${YEL}»» git, ansible telepítése......${NC}"
     sudo apt-add-repository ppa:ansible/ansible -y
     sudo apt update -y
 	sudo apt install git software-properties-common ansible -y
-    echo "${YEL}apt autoclean......${NC}"
+    echo "${YEL}»» apt autoclean......${NC}"
     sudo apt autoclean -y && sudo apt autoremove -y
     echo "${YEL}Add meg a MySQL jelszavad [bármi lehet]:${NC}"
     read sqljelszo
